2d04c07a2251b6639361b3a0b2441ede4aa0229b
Vince Laquindanum <laq.vince@gmail.com>
2021-06-16 07:28:50 -0700

    Defines winning combinations

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// Defines winning combinations
+const winCombos = [
+    // Rows
+    [0,1,2],[3,4,5],[6,7,8],
+    // Columns
+    [0,3,6],[1,4,7],[2,5,6],
+    // Diagonal
+    [0,4,8],[2,4,6]
+];
